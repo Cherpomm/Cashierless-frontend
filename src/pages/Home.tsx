4abf958a1848674ca78d530b948a015afdccdf2e
Supakit Lokaew <63579015+Cherpomm@users.noreply.github.com>
2023-01-23 17:02:40 +0700

    scss change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Home: React.FC = () => {
   return (
-    <Row className="py-3 px-4">
+    <Row className="py-3 px-4 align-items-center">
       <Col md>
         <h1 className="fw-bold display-5">
           Hello!! <br /> Want To Experience <br /> Contact-Less Store?
